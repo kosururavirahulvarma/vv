ab58a74583e4a57320995212a4c9695208354d26
ravi.kosuru <ravi.kosuru@192.168.60.30>
2023-06-23 18:01:25 +0530

    new c1

---
+++ src/main/java/com/security/jwt/entity/Food2.java
@@ -0,0 +1,92 @@
+package com.security.jwt.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity
+
+public class Food2 {
+	@Id
+	private Integer id;
+
+	private String name;
+
+	private String description;
+
+	private String category;
+
+	private Double price;
+
+	private Boolean vegetarian;
+
+	public Food2() {
+		super();
+	}
+
+	public Food2(Integer id, String name, String description, String category, Double price, Boolean vegetarian) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.category = category;
+		this.price = price;
+		this.vegetarian = vegetarian;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Boolean isVegetarian() {
+		return vegetarian;
+	}
+
+	public void setVegetarian(Boolean vegetarian) {
+		this.vegetarian = vegetarian;
+	}
+
+	@Override
+	public String toString() {
+		return "Food [id=" + id + ", name=" + name + ", description=" + description + ", category=" + category
+				+ ", price=" + price + ", vegetarian=" + vegetarian + "]";
+	}
+}
